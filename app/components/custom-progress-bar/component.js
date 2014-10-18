bfbbafe22d2182d09319edb2f0c5141fc918b899
Gaurav Munjal <Gaurav0@aol.com>
2014-10-18 12:07:50 -0700

    Add customized progress bar

---
+++ app/components/custom-progress-bar/component.js
@@ -0,0 +1,24 @@
+import Ember from 'ember';
+import JquiProgressBarComponent from 'ember-cli-jquery-ui/components/jqui-progress-bar/component';
+
+export default JquiProgressBarComponent.extend({
+    value: 0,
+    max: 100,
+    text: "0%",
+    animate: function() {
+        if (this.get('value') < this.get('max')) {
+            this.incrementProperty('value');
+            this.set('text', this.get('value') + "%");
+        }
+        var _this = this;
+        Ember.run.later(function() {
+            _this.animate();
+        }, 30);
+    }.on('didInsertElement'),
+    complete: function() {
+        var _this = this;
+        Ember.run.next(function() {
+            _this.set('text', "Complete!");
+        });
+    }
+});
