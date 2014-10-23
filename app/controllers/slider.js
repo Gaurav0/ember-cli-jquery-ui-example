cf391bf21435e771eedf6b42ae9e8558ab3d18cf
Gaurav Munjal <Gaurav0@aol.com>
2014-10-22 18:24:47 -0700

    Actions with sliders

--- app/controllers/slider.js
+++ app/controllers/slider.js
@@ -2,5 +2,15 @@ import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
     num: 50,
-    vals: [10, 90]
+    vals: [10, 90],
+    text: "50%",
+    num2: 50,
+    didSlideBasic: function() {
+        this.set('num2', this.get('num'));
+    }.observes('num'),
+    actions: {
+        didSlide: function(event, ui) {
+            this.set('text', ui.value + "%");
+        }
+    }
 });
