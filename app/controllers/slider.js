ed7d8d909e8afeb76ad3c4e54df519ec20f92809
Gaurav Munjal <Gaurav0@aol.com>
2014-11-05 20:31:23 -0800

    Update to Ember-cli 0.1.2 and Ember.js 1.8.1

--- app/controllers/slider.js
+++ app/controllers/slider.js
@@ -5,13 +5,17 @@ export default Ember.ObjectController.extend({
     vals: [10, 90],
     num2: 50,
     num3: 50,
-    text: "",
+    underscores: "__",
+    containerName: function() {
+        var underscores = this.get('underscores');
+        return "App." + underscores + "container" + underscores;
+    }.property('underscores'),
     didSlideBasic: function() {
         this.set('num2', this.get('num'));
     }.observes('num'),
     actions: {
         didSlide: function() {
-            this.set('text', this.get('text') + "_");
+            this.set('underscores', this.get('underscores') + "_");
         }
     }
 });
