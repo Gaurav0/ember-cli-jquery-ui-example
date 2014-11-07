d263e3b35f17455dd7dae3d7340f26c6bb83f69e
Gaurav Munjal <Gaurav0@aol.com>
2014-11-07 02:43:52 -0800

    Add accordion widget

--- app/router.js
+++ app/router.js
@@ -6,6 +6,7 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+    this.route('accordion');
     this.route('autocomplete');
     this.route('button');
     this.route('datepicker');
